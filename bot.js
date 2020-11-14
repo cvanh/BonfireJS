1f04b05228aea42acfe36799f078a54c48b6cd9f
Giotje_ <531406@student.glu.nl>
2020-11-14 14:35:56 +0100

    [feature] setup database and guildCreate

--- bot.js
+++ bot.js
@@ -3,8 +3,15 @@ const fs = require("fs");
 const bot = new Discord.Client();
 const botconfig = require("./botconfig.json");
 bot.commands = new Discord.Collection();
-let prefix = botconfig.prefix;
 require("dotenv").config();
+const mysql = require("mysql");
+const con = mysql.createConnection(process.env.JAWSDB_URL);
+let prefix = botconfig.prefix;
+
+con.connect((e) => {
+  if (e) console.error(e);
+  console.log("Connected to database ^^");
+});
 fs.readdir("./commands", (err, files) => {
   if (err) console.error(err);
   let jsfile = files.filter((f) => f.split(".").pop() === "js");
@@ -28,6 +35,7 @@ bot.on("ready", async () => {
   });
 });
 
+// Basic setup
 bot.on("message", async (message) => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
@@ -46,6 +54,53 @@ bot.on("message", async (message) => {
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if (commandfile) commandfile.run(bot, message, args);
+});
+
+//Add Channels
+bot.on("guildCreate", async (guild) => {
+  if (!guild.channels.exists("name", "bonfire")) {
+    $: category = await guild.createChannel("bonfire", {
+      type: "category",
+      permissionOverwrites: [
+        {
+          id: guild.id,
+          deny: [
+            "SEND_MESSAGES",
+            "SEND_TTS_MESSAGES",
+            "MANAGE_MESSAGES",
+            "ATTACH_FILES",
+            "ADD_REACTIONS",
+            "VIEW_CHANNEL",
+          ],
+        },
+      ],
+    });
+  }
+  if (!guild.channels.exists("name", "🏕️")) {
+    $: channel = await guild.createChannel("🏕️", {
+      type: "text",
+      permissionOverwrites: [
+        {
+          id: guild.id,
+          deny: [
+            "SEND_MESSAGES",
+            "SEND_TTS_MESSAGES",
+            "MANAGE_MESSAGES",
+            "ATTACH_FILES",
+            "ADD_REACTIONS",
+          ],
+        },
+      ],
+    });
+  }
+
+  await channel.setParent(category);
+
+  await channel.lockPermissions();
+
+  await channel.sendMessage(
+    `hello! thank you so much for adding me to your server. In this channel my developer will post status updated and other important things. if you find any bugs don't be afraid to report them through the github repository (https://github.com/app-bonfire/BonfireJS/issues) or by adding my developer (gio#1234). stay cozy. This channel is only visible for administratos ^-^`
+  );
 });
 
 bot.login(process.env.TOKEN);
